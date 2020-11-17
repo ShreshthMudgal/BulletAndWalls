a9bc263300e0549c9cdc321d6f4b255f2cd97fe0
ShreshthMudgal <73778370+ShreshthMudgal@users.noreply.github.com>
2020-11-17 19:58:50 +0530

    Add files via upload

---
+++ myLibrary.js
@@ -0,0 +1,17 @@
+
+function hasCollided(lBullet,lWall)
+{
+bulletRightEdge=lBullet.x +lBullet.width;
+
+wallLeftEdge=lWall.x;
+
+if(bulletRightEdge>=wallLeftEdge)
+{
+
+return true;
+
+}
+
+return false;
+
+}
